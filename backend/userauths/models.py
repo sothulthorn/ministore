d6bd31153c3910fda765bf4d3bf93c1e30045ab9
sothulthorn <thornsothul@gmail.com>
2024-02-09 16:44:38 +0900

    Signals In Django

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 from shortuuid.django_fields import ShortUUIDField
 
 # Create your models here.
@@ -25,7 +26,7 @@ class User(AbstractUser):
     super(User, self).save(*args, **kwargs)
 
 class Profile(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   image = models.FileField(upload_to='image', default='default/default-user.jpg', null=True, blank=True)
   full_name = models.CharField(max_length=100, null=True, blank=True)
   about = models.TextField(null=True, blank=True)
@@ -48,4 +49,14 @@ class Profile(models.Model):
       self.full_name = self.user.full_name
     
     super(Profile, self).save(*args, **kwargs)
+
+  def create_user_profile(sender, instance, created, **kwwargs):
+    if created:
+      Profile.objects.create(user=instance)
+
+  def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+  post_save.connect(create_user_profile, sender=User)
+  post_save.connect(save_user_profile, sender=User)
 
